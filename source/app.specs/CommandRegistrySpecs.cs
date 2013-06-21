0f3dd3fb17cddc2c5f57967964a3a7e1933993be
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-06-20 19:42:45 -0600

    Updated

--- source/app.specs/CommandRegistrySpecs.cs
+++ source/app.specs/CommandRegistrySpecs.cs
@@ -43,6 +43,35 @@ namespace app.specs
         static IContainRequestInformation request;
         static IList<IProcessOneRequest> all_the_commands;
       }
+
+      public class and_it_does_not_have_the_command
+      {
+        Establish c = () =>
+        {
+          the_missing_request_command = fake.an<IProcessOneRequest>();
+          all_the_commands = new List<IProcessOneRequest>();
+          depends.on<IEnumerable<IProcessOneRequest>>(all_the_commands);
+
+          depends.on<ICreateTheMissingRequestCommand>(x =>
+          {
+            x.ShouldEqual(request);
+            return the_missing_request_command;
+          });
+
+          request = fake.an<IContainRequestInformation>();
+        };
+
+        Because b = () =>
+          result = sut.get_command_that_can_process(request);
+
+        It should_return_the_missing_request_command_to_the_caller = () =>
+          result.ShouldEqual(the_missing_request_command);
+
+        static IProcessOneRequest result;
+        static IProcessOneRequest the_missing_request_command;
+        static IContainRequestInformation request;
+        static IList<IProcessOneRequest> all_the_commands;
+      }
     }
   }
 }
