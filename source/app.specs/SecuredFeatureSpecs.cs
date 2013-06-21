bfb009b70e8cdd01d691175b36991318082deb15
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-06-20 22:49:27 -0600

    Updated

---
+++ source/app.specs/SecuredFeatureSpecs.cs
@@ -0,0 +1,42 @@
+﻿ using Machine.Specifications;
+ using app.web.core;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{  
+  [Subject(typeof(SecuredFeature))]  
+  public class SecuredFeatureSpecs
+  {
+    public abstract class concern : Observes<IImplementAFeature,
+                                      SecuredFeature>
+    {
+        
+    }
+
+   
+    public class when_run : concern
+    {
+      public class and_the_security_constraint_is_satisfied
+      {
+        Establish c = () =>
+        {
+          depends.on<IVerifyASecurityConstraint>(() => true);
+          actual_feature = depends.on<IImplementAFeature>();
+          request = fake.an<IContainRequestInformation>();
+        };
+
+        Because b = () =>
+          sut.process(request);
+
+        It should_run_the_actual_feature = () =>
+          actual_feature.received(x => x.process(request));
+
+        static IImplementAFeature actual_feature;
+        static IContainRequestInformation request;
+      }
+        
+        
+    }
+  }
+}
---
+++ source/app/web/core/SecuredFeature.cs
@@ -0,0 +1,21 @@
+﻿namespace app.web.core
+{
+  public class SecuredFeature : IImplementAFeature
+  {
+    IImplementAFeature actual_feature;
+    IVerifyASecurityConstraint security_specification;
+
+    public SecuredFeature(IImplementAFeature actual_feature, IVerifyASecurityConstraint security_specification)
+    {
+      this.actual_feature = actual_feature;
+      this.security_specification = security_specification;
+    }
+
+    public void process(IContainRequestInformation request)
+    {
+        if (security_specification()) 
+          actual_feature.process(request);
+
+    }
+  }
+}
