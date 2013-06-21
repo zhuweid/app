75b3590e83bb2f15860b78141a79a40b7d98a553
Raghavendra Prasad K <rprasadk@gmail.com>
2013-06-21 13:35:28 +1000

    Pushing new changes

--- source/app/web/application/catalogbrowsing/ViewTheMainDepartmentInTheStore.cs
+++ source/app/web/application/catalogbrowsing/ViewTheMainDepartmentInTheStore.cs
@@ -5,15 +5,19 @@ namespace app.web.application.catalogbrowsing
   public class ViewTheMainDepartmentInTheStore : IImplementAFeature
   {
     IFindDepartments department_repository;
+      private readonly IDisplayInformation display_engine;
 
-    public ViewTheMainDepartmentInTheStore(IFindDepartments department_repository)
+      public ViewTheMainDepartmentInTheStore(IFindDepartments department_repository, IDisplayInformation display_engine)
     {
-      this.department_repository = department_repository;
+        this.department_repository = department_repository;
+        this.display_engine = display_engine;
     }
 
     public void process(IContainRequestInformation request)
     {
-      department_repository.get_the_main_departments();
+      var main_departments = department_repository.get_the_main_departments();
+
+      display_engine.display_the_main_departments(main_departments);
     }
   }
 }
