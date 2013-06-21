080931ce016a1c019db8bb5e15051679f804260b
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-06-20 19:49:52 -0600

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace app.web.core
 {
   public class CommandRegistry : IFindCommands
   {
-      private readonly IEnumerable<IProcessOneRequest> commands;
+    IEnumerable<IProcessOneRequest> commands;
 
-      public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
-      {
-          this.commands = commands;
-      }
+    public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+    {
+      this.commands = commands;
+    }
 
     public IProcessOneRequest get_command_that_can_process(IContainRequestInformation request)
     {
-        var foundCommand = commands.FirstOrDefault(c => c.can_process(request));
-        if (foundCommand == null)
-            throw new InvalidOperationException("command not found.");
+      var command = commands.First(c => c.can_process(request));
 
-        return foundCommand;
+      return command;
     }
   }
 }
